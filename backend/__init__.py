2081ad4e8d3c8368cebb2213333437656599bef3
Asadik Hamed <asadik.hamed68@gmail.com>
2026-08-13 11:58:26 +0400

    Update __init__.py

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,5 +1,5 @@
-"""UAEU Libraries AI Assistant backend package."""
-"""UAEU Libraries AI Assistant backend.
+""" Libraries AI Assistant backend package."""
+"" Libraries AI Assistant backend.
 
 Copyright (c) 2026 Asadik Hamed. All rights reserved. See LICENSE.
 """
